faf19b995b166f2dcbc2020039eee9c3673ee21c
lcb931023 <lcb931023@gmail.com>
2017-05-02 17:04:02 -0700

    detect when worms are close; mingle when they are close

--- index.js
+++ index.js
@@ -7,15 +7,16 @@ var Worm = require('./lib/worm');
 // multi-mesh worm for debugging purpose that likely will make into final build
 var LongWorm = require('./lib/longWorm');
 
-const BOUND_SIZE = 50;
+let BOUND_SIZE = 20;
 const WORM_SCALE = 0.5;
-const RECORDING_DURATION = 3;
+const RECORDING_DURATION = 1;
+const INTIMATE_DIST = 10;
 
 var app = createOrbitViewer({
-    clearColor: 0x000000,
-    clearAlpha: 1,
-    fov: 65,
-    position: new THREE.Vector3(0, 0, 70),
+  clearColor: 0x000000,
+  clearAlpha: 1,
+  fov: 65,
+  position: new THREE.Vector3(0, 0, 70),
 })
 
 app.scene.add(new THREE.Mesh(
@@ -30,6 +31,7 @@ function onRecordEnd(rec) {
   // Generate a worm that keeps track of the recording
   var worm = new LongWorm(
     rec.exportBuffer(),
+    RECORDING_DURATION,
     (Math.random() - .5) * BOUND_SIZE, 
     (Math.random() - .5) * BOUND_SIZE, 
     (Math.random() - .5) * BOUND_SIZE,
@@ -55,11 +57,30 @@ document.addEventListener('keyup', (e) => {
 });
 
 app.on('tick', function(dt) {
-    //.. handle pre-render updates    
-    arrWorm.forEach((worm)=>{
-      // TODO move these into worm class's update
-      worm.wander();
-      worm.bounce(BOUND_SIZE * 2, BOUND_SIZE * 2, BOUND_SIZE * 2);
-      worm.update();
-    });
+  //.. handle pre-render updates    
+  arrWorm.forEach((worm)=>{
+    // TODO move these into worm class's update
+    worm.wander();
+    worm.bounce(BOUND_SIZE);
+    worm.update();
+  });
 })
+
+setInterval(detectIntimacy, 200);
+/**
+* NOTE crude detection of intimacy
+* @param bI: base worm's index
+* @param tI: target worm's index
+* O( (n-1)*n/2 ), where n is arrWorm.length
+*/
+function detectIntimacy() {
+  for (let bI = 0; bI < arrWorm.length - 1; bI++) {
+    for (let tI = bI + 1; tI < arrWorm.length; tI++) {
+      var dist = arrWorm[bI].distanceTo(arrWorm[tI]);
+      if (dist < INTIMATE_DIST) {
+        arrWorm[bI].play();
+        arrWorm[tI].play(); // TODO play with delaying this worm
+      }
+    }
+  }
+}
